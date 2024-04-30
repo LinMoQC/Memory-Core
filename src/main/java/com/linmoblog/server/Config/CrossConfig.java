10fe00d870ab2cd6f915d05ef789b991cc44193c
Karensky <2896311434@qq.com>
2024-04-30 21:34:48 +0800

    update

--- src/main/java/com/linmoblog/server/Config/CrossConfig.java
+++ src/main/java/com/linmoblog/server/Config/CrossConfig.java
@@ -14,7 +14,7 @@ public class CrossConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://127.0.0.1")
+                .allowedOrigins("http://127.0.0.1:81")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .maxAge(3600);
     }
